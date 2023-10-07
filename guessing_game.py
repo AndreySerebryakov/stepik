60e9eb9586f211d74f0d129ddc20bdf09dc05d27
AndreySerebryakov <andreyserebryakovv1@gmail.com>
2023-10-07 21:05:58 +0300

    create mini-game

---
+++ guessing_game.py
@@ -0,0 +1,20 @@
+from random import *
+
+
+def game():
+    answer = ['Вы угадали, поздравляем!', 'Слишком много, попробуйте еще раз',
+              'Слишком мало, попробуйте еще раз']
+    num = randint(1, 100)
+    while True:
+        n = int(input())
+        if n == num:
+            print(answer[0])
+            break
+        elif n > num:
+            print(answer[1])
+            continue
+        else:
+            print(answer[2])
+            continue
+
+game()
