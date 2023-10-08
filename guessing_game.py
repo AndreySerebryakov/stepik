4c460ce75b6a638572bbeca0d9cf886f282a7c7e
AndreySerebryakov <andreyserebryakovv1@gmail.com>
2023-10-08 12:13:15 +0300

    added number chek

--- guessing_game.py
+++ guessing_game.py
@@ -6,16 +6,24 @@ def game():
               'Слишком мало, попробуйте еще раз']
     num = randint(1, 100)
     while True:
-        n = int(input())
-        if n == num:
-            print(answer[0])
-            break
-        elif n > num:
-            print(answer[1])
-            continue
+        n = input()
+        if is_valid(n):
+            n = int(n)
+            if n == num:
+                print(answer[0])
+                break
+            elif n > num:
+                print(answer[1])
+                continue
+            else:
+                print(answer[2])
+                continue
         else:
-            print(answer[2])
-            continue
+            print('Проверьте введенные данные. Число должно быть от 1 до 100!')
+
+
+def is_valid(number):
+    return number.isdigit() and 1 <= int(number) <= 100
 
 
 game()
